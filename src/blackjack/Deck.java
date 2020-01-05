897ec77a623a24c24808b8d8805879935b25980a
ibradeveloper1 <44679841+ibradeveloper1@users.noreply.github.com>
2020-01-04 21:54:44 -0500

    'message'

---
+++ src/blackjack/Deck.java
@@ -0,0 +1,36 @@
+package blackjack;
+
+import blackjack.Card.Rank;
+import blackjack.Card.Suit;
+/**
+ * Standard 52 card deck
+ *
+ * @author Louise Rennick 
+ */
+public class Deck {
+
+    private Card[] cards = new Card[52];
+
+    public Deck() {
+        refill();
+    }
+
+    public final void refill() {
+        int i = 0;
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards[i++] = new Card(suit, rank);
+            }
+        }
+    }
+
+    public Card drawCard() {
+        Card card = null;
+        while (card == null) {
+            int index = (int)(Math.random()*cards.length);
+            card = cards[index];
+            cards[index] = null;
+        }
+        return card;
+    }
+}
